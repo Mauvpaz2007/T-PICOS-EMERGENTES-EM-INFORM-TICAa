63f6cb17b34156aadff356a2f7eefd657c557a16
Mauvpaz2007 <Mauvpaz@gmail.com>
2025-05-12 15:03:30 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,53 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Agenda </title>
-   <link rel="stylesheet" href="estilo.css">
-</head>
+<html>
+
 <body>
-     <H1 id="h1"> EXEMPLO - AGENDA<H1>
- 
-<form action= "salva.php" method= "post">
-        Nome: <input type="text" id="nome" placeholder="Digite seu nome">
-        <br><br>
-        Endereço: <input type="text" id="endereco" placeholder="Digite seu endereco"> 
-        <br><br>
-        Telefone: <input type="text" id="telefone" placeholder="Digite seu número">
-        <br><br>
-        <input type="submit" value="Cadastrar"> <br><br>
+    <h2>Cadastrar Contato</h2>
 
- <?php
+    <form method="post" action="salvar.php">
 
-            $servidor = "localhost";
-            $user = "root";
-            $senha = "";
-            $banco = "agenda";
-            $conexao = mysqli_connect($servidor,$user,$senha,$banco);
-            if(!$conexao){
-                    echo "ERRO DE CONEXÃO";
-          
-            }
-           
-           
-?>
-<?php
+        Nome: <input type="text" name="nome" required><br><br>
+        Endereço: <input type="text" name="endereco"><br><br>
+        Telefone: <input type="text" name="telefone"><br><br>
+        <input type="submit" value="Salvar">
+    </form>
 
+    <h2>Lista de Contatos</h2>
+    <?php
+    include('conexao.php');
 
-    $sqli = "select * from contatos";
-    $res = mysqli_query($conexao,$sqli);
-    if(mysqli_num_rows($res)>0){
-        while($linha= mysqli_fetch_assoc($res)){
-            echo "<br>". $linha['nome']."<br>".$linha['endereco']."<br>".$linha['telefone']."<a href='edita.php'?id=" . $linha["id"] . "'>Editar</a> <a href='exclui.php?id=" . $linha["id"] . "'>Excluir</a><br>"
-;        }
+    $sql = "SELECT * FROM contatos";
 
-    }else{
-        echo "Nenhum contato encontrado";
+    $resultado = mysqli_query($conexao, $sql);
+
+    if (mysqli_num_rows($resultado) > 0) {
+        while ($linha = mysqli_fetch_assoc($resultado)) {
+            echo $linha['nome'] . " | " . $linha['endereco'] . " | " . $linha['telefone'] .
+                " | <a href='editar.php?id=" . $linha["id"] . "'>Editar</a>" .
+                " | <a href='excluir.php?id=" . $linha["id"] . "'>Excluir</a> <br>";
+        }
+    } else {
+        echo "Nenhum contato encontrado.";
     }
-?>
-
-  
+    ?>
 </body>
 
 </html>
